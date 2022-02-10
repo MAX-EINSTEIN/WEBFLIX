819bd6cd933d49f3496ae78604529aadc7a69d36
Junaid Siddiqui <maxeinstein.dev@gmail.com>
2022-02-10 10:48:39 +0530

    Created Video Model and Models Proxies for All Videos and Published Videos

--- backend/webflix/apps/catalogue/items/videos/admin.py
+++ backend/webflix/apps/catalogue/items/videos/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import VideoAllProxy, VideoPublishedProxy
+
 
 # Register your models here.
+
+
+class VideoAllAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'state', 'embed_id', 'is_published']
+    search_fields = ['title', 'description']
+    list_filter = ['state', 'active']
+    readonly_fields = ['id', 'slug', 'is_published', 'publish_timestamp']
+
+    class Meta:
+        model = VideoAllProxy
+
+
+admin.site.register(VideoAllProxy, VideoAllAdmin)
+
+
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'slug', 'embed_id']
+    search_fields = ['title', 'description']
+    readonly_fields = ['id', 'slug', 'is_published', 'publish_timestamp']
+
+    class Meta:
+        model = VideoPublishedProxy
+
+    def get_queryset(self, request):
+        return VideoAllProxy.objects.filter(active=True)
+
+
+admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
--- backend/webflix/apps/catalogue/items/videos/models.py
+++ backend/webflix/apps/catalogue/items/videos/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.utils import timezone
+from django.db.models.signals import pre_save
+from webflix.utils.models import PublishStateOptions
+from webflix.utils.receivers import slugify_pre_save, publish_state_pre_save
 
 # Create your models here.
+
+
+class VideoQuerySet(models.QuerySet):
+    def published(self):
+        now = timezone.now()
+        return self.filter(
+            state=PublishStateOptions.PUBLISH,
+            publish_timestamp__lte=now
+        )
+
+
+class VideoManager(models.Manager):
+    def get_queryset(self):
+        return VideoQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True)
+    embed_id = models.CharField(max_length=256)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    state = models.CharField(
+        max_length=2, choices=PublishStateOptions.choices, default=PublishStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(
+        auto_now=False, auto_now_add=False, blank=True, null=True)
+
+    objects = VideoManager()
+
+    @property
+    def is_published(self):
+        return self.active
+
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
+
+
+class VideoPublishedProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
+
+
+pre_save.connect(publish_state_pre_save, sender=Video)
+pre_save.connect(slugify_pre_save, sender=Video)
